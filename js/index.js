40862a55352b2bddf8adb1cc2b6e12d5df0ba5ac
Woranut Dangworawith <146110465+danworanut@users.noreply.github.com>
2023-11-30 16:11:49 +0700

    sticky header

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
 // Active navbar link
 
-var menu = document.getElementsByClassName("menu");
-
-
 var listMenus = document.getElementsByClassName("menu-list")
 
 for (let i = 0; i < listMenus.length; i++) 
@@ -19,5 +16,20 @@ for (let i = 0; i < listMenus.length; i++)
 
         this.className += " active";
     })
-    
+     
+}
+
+// sticky header 
+
+window.onscroll = function() {stickyHeader()};
+
+var header = document.querySelector("header")
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
